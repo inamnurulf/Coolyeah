b65ddc296d828ffec5b03236a64635b92482492e
rhafif-p <rhafif.prasetyo@mail.ugm.ac.id>
2023-09-21 18:04:38 +0700

    add: Food class

---
+++ Coolyeah/Coolyeah/Food.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coolyeah
+{
+    class Food
+    {
+        // Private attributes
+        private string _notes;
+        private int _value;
+
+        // Public properties
+        public string Notes
+        {
+            get { return _notes; }
+            set { _notes = value; }
+        }
+
+        public int Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+
+        // Constructor
+        public Food(string notes, int value)
+        {
+            _notes = notes;
+            _value = value;
+        }
+
+        // Function to increase the Value
+        public void Increase()
+        {
+
+        }
+
+        // Function to decrease the Value
+        public void Decrease()
+        {
+
+        }
+    }
+
+}
