9b21f062d9e9020bb0311175670d800c7caed3a7
giftormdh <giftoramadha@gmail.com>
2023-09-21 14:25:44 +0700

    add : Drink and Sleep Class

---
+++ Coolyeah/Coolyeah/Drink.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coolyeah
+{
+    class Drink
+    {
+        //Mendefinisikan Instance variable
+        private int _value;
+
+        //Mendefinisikan Property
+        public int Value { get { return _value; } set { _value = value; } }
+
+        //Mendefinisikan method kelas
+        public int Increase()
+        {
+            return _value ++;
+        }
+        public int Decrease()
+        {
+            return _value--;
+        }
+    }
+}
---
+++ Coolyeah/Coolyeah/Sleep.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Coolyeah
+{
+    class Sleep
+    {
+        //Mendefinisikan Instance variable
+        private string _notes;
+        private DateTime _start;
+        private DateTime _end;
+
+        //Mendefinisikan Property
+        public string Notes { get { return _notes; } set { _notes = value; } }
+        public DateTime Start { get { return _start; } set { _start = value; } }
+        public DateTime End { get { return _end; } set { _end = value; } }
+
+        //Mendefinisikan method kelas
+        public string setValue(DateTime Start, DateTime End)
+        {
+            _start = Start; _end = End;
+            return _notes;
+        }
+    }
+}
