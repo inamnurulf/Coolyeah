09a397d75d0fb805d32ae3444f708c4b0462ab31
inamnurul <aam.inam7310@gmail.com>
2023-11-16 02:25:33 +0700

    Initiate model

---
+++ Coolyeah-App/Models/Sleep.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coolyeah_App.Models
+{
+    class Sleep
+    {
+        public int id { get; set; }
+        public DateTime TimeStart { get; set; }
+        public DateTime TimeEnd { get; set; }
+    }
+}
