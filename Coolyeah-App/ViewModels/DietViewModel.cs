a2182bfc8c47cb495f455fff3fa7e455367fd917
inamnurul <aam.inam7310@gmail.com>
2023-11-16 19:57:01 +0700

    feat : data binding test

--- Coolyeah-App/ViewModels/DietViewModel.cs
+++ Coolyeah-App/ViewModels/DietViewModel.cs
@@ -1,12 +1,51 @@
-﻿using System;
+﻿using Coolyeah_App.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Coolyeah_App.ViewModels
 {
     class DietViewModel
     {
+        public ICommand AddNewItemCommand { get; set; }
+
+        public ObservableCollection<Food> MyDataCollection { get; set; }
+
+
+        public DietViewModel()
+        {
+            AddNewItemCommand = new RelayCommand(AddNewItem);
+            MyDataCollection = new ObservableCollection<Food>();
+            FetchData();
+        }
+        private void FetchData()
+        {
+            var apiData = FetchDataFromApi();
+
+            foreach (var item in apiData)
+            {
+                MyDataCollection.Add(item);
+            }
+        }
+
+        private IEnumerable<Food> FetchDataFromApi()
+        {
+            return new List<Food>
+            {
+                new Food { id = 1, Notes = "Value1", Value=2 },
+                new Food { id = 2, Notes = "Value2", Value=5 },
+                new Food { id = 3, Notes = "Value3", Value=4 },
+                new Food { id = 4, Notes = "Value4", Value=2 },
+            };
+        }
+        private void AddNewItem(object parameter)
+        {
+            MessageBox.Show("Adding new item");
+        }
     }
 }
--- Coolyeah-App/Views/DietView.xaml.cs
+++ Coolyeah-App/Views/DietView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Coolyeah_App.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace Coolyeah_App.Views
         public DietView()
         {
             InitializeComponent();
+            DataContext = new DietViewModel();
+
         }
     }
 }
